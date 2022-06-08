69c272bfe1c0b2aa2d60fb3781cd07d08489b50f
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-06-08 15:00:01 +0700

    developed fun play

--- hangman.py
+++ hangman.py
@@ -12,35 +12,44 @@ def play(word, lives):
     :param lives:(int) number limiting mistakes
     """
     print(word) # for testing purposes to be removed
+    word_set = set(word)
+    #word_set.remove('\n')
+    print(word_set)
+    guess_set = set()
     print('Word: ', end='')
     for char in word:
-        print('_', end='')
+        if char not in guess_set:
+            print('_', end='')
+        else:
+            print(char, end='')
 
     print('\nLives: ', end='')
     print('@' * lives)
-    guess_set = set()
 
-    while lives > 0:
+    while lives >= 0:
         letter = input('choose letter: ')
         if not letter.isalpha():
             incorrect_input("Input must be single letter.")
         elif len(letter) != 1:
             incorrect_input('Type one latin alphabet letter')
         else:
-            guess_set.add(letter)
-            if letter in word:
-                for char in word:
-                    if char != letter:
-                        print('_', end='')
-                    else:
-                        print(letter, end='')
-                print('\nLives: ', end='')
-                print('@' * lives)
-                guess_set.add(letter)
+            if letter in guess_set:
+                print(f'The letter: "{letter}" you tried already. Try another one!')
             else:
-                lives -= 1
-                print('\nLives: ', end='')
-                print('@' * lives)
+                guess_set.add(letter)
+                if letter in word:
+                    for char in word:
+                        if char not in guess_set:
+                            print('_', end='')
+                        else:
+                            print(char, end='')
+                    print('\nLives: ', end='')
+                    print('@' * lives)
+                    guess_set.add(letter)
+                else:
+                    lives -= 1
+                    print('\nLives: ', end='')
+                    print('@' * lives)
 
 
 def random_word():
@@ -55,11 +64,12 @@ def random_word():
     with open('countries-and-capitals.txt', 'r') as words:
         words_list = words.readlines()
         word = random.choice(words_list)
+        word = word.strip('\n')
     return word
 
 
 def incorrect_input(message):
-    print('Invalid Input. Select number from Menu')
+    print('Invalid Input.')
     print(message)
 
 
