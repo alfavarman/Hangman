b99c1ee53e89b93c580f1bdbd93cc277f788f21f
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-06-12 02:27:18 +0700

    improved code

--- hangman.py
+++ hangman.py
@@ -20,15 +20,15 @@ def play(word, lives):
     :param word:(str) word to guess
     :param lives:(int) number limiting mistakes
     """
-    os.system('clear')
+    clrscr()
     print(word)  # for testing purposes to be removed
     word_set = set(word.casefold())
     guess_set = set()
-    # max_lives = lives
 
     while lives > -1:
+        clrscr()
         load_graphic(lives)
-        print('\t\t\t', end='')
+        print('\t\tWORD: ', end='')
         for char in word:
             if char.casefold() not in guess_set:
                 print('_', end='')
@@ -45,20 +45,29 @@ def play(word, lives):
         if not letter.isalpha():
             incorrect_input("Input must be single letter.")
         elif letter.casefold() == 'quit':
+            good_bye(1)
+            clrscr()
+            break
+        elif letter.casefold() == 'exit':
+            good_bye(1)
+            clrscr()
             break
         elif len(letter) != 1:
             incorrect_input('Type one latin alphabet letter')
         else:
             if letter in guess_set:
                 print(f'The letter: "{letter}" you tried already. Try another one!')
+                sleep(2)
             else:
                 guess_set.add(letter)
                 if letter not in word_set:
                     lives -= 1
         if lives == 0:
+            clrscr()
             load_graphic(0)
-            print('Sorry Your Friend is Hanged!')
+            print('\t\tSorry Your Friend is Hanged!')
             sleep(3)
+            clrscr()
             break
 
 
@@ -81,6 +90,7 @@ def random_word():
 def incorrect_input(message):
     print('Invalid Input.')
     print(message)
+    sleep(2.5)
 
 
 def good_bye(sec: int = 3):
@@ -92,23 +102,28 @@ def good_bye(sec: int = 3):
 def print_menu():
     """function to print menu"""
     # bug: invalid input at menu(Hard Medium Easy) moves back to main menu - should stay same menu)
+    # need to open another while loop instead of if statement.
     clrscr()
     while True:
         choice = input('''
-        Welcome to Hangman Countries & Capitols
-        ***************************************
-        1: Play
-        2: Manual
-        3: Exit
+                Welcome to Hangman Countries & Capitols
+                ***************************************
+            
+                1: Play
+                2: Manual
+                3: Exit
         ''')
         if choice == '1':
             clrscr()
             choice2 = input("""
-        1: Hard
-        2: Medium
-        3: Easy
-        
-        4: Back
+                Welcome to Hangman Countries & Capitols
+                ***************************************
+                
+                1: Hard
+                2: Medium
+                3: Easy
+                
+                4: Back
         """)
             if choice2 == '1':
                 play(random_word(), 3)
@@ -119,24 +134,25 @@ def print_menu():
             elif choice2 == '4':
                 pass
             elif choice2.casefold() == 'quit':
-                good_bye()
+                good_bye(1)
+                break
+            elif choice.casefold() == 'exit':
+                good_bye(1)
                 break
             else:
                 incorrect_input('select number from menu or type quit')
         elif choice == '2':
             print('\t' + manual)
             input('\t\tpress any key to go back')
+            clrscr()
         elif choice == '3':
-            good_bye()
+            good_bye(1)
             break
-        elif choice.casefold == 'quit':
-            good_bye()
+        elif choice.casefold() == 'quit':
+            good_bye(1)
+            break
+        elif choice.casefold() == 'exit':
+            good_bye(1)
             break
         else:
             incorrect_input('select from menu')
-
-
-if __name__ == '__main__':
-    print_menu()
-
-
